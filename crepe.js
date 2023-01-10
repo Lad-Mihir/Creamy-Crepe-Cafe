8548869cbdf5c765b90024e82da41abc51ff1f65
Lad-Mihir <mihir17295@gmail.com>
2023-01-10 16:59:04 -0500

    Mihir Lad

---
+++ crepe.js
@@ -0,0 +1,11 @@
+
+$(document).ready(() => {
+    // $("#Card1").addEventListener("mouseover", evt=> {
+    //     $("#card1_h3").next().text("Loren Ipsum...");
+    // });
+
+
+});
+function onhover() {
+    $("#card1_h3").next().text("Lorem Ipsum...");
+ }
