a27a1eddeb13c82e6a1a3f8038ba8f60ed32d3ea
Lad-Mihir <mihir17295@gmail.com>
2023-01-13 21:39:58 -0500

    Mihir lad

--- crepe.js
+++ crepe.js
@@ -18,4 +18,45 @@ function onhover2() {
 function onout2() {
    $("#card2_h3").next().text("");
 }
-
+function onhover3() {
+   $("#card3_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout3() {
+   $("#card3_h3").next().text("");
+}
+function onhover4() {
+   $("#card4_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout4() {
+   $("#card4_h3").next().text("");
+}
+function onhover5() {
+   $("#card5_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout5() {
+   $("#card5_h3").next().text("");
+}
+function onhover6() {
+   $("#card6_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout6() {
+   $("#card6_h3").next().text("");
+}
+function onhover7() {
+   $("#card7_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout7() {
+   $("#card7_h3").next().text("");
+}
+function onhover8() {
+   $("#card8_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout8() {
+   $("#card8_h3").next().text("");
+}
+function onhover9() {
+   $("#card9_h3").next().text("Lorem Ipsum ipsum...");
+}
+function onout9() {
+   $("#card9_h3").next().text("");
+}
